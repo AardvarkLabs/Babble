95bda379a159bd2df2ae79f1f42394630b82601e
Felipe Alfonso González <f.alfonso.go@gmail.com>
2017-05-24 03:37:20 -0400

    v4.3.2.0.1

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const path = require('path');
-//build in node module , no need to install it
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
@@ -14,6 +13,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
