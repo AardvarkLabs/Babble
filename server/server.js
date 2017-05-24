fd5b3701fb89523c20c990b0cb0fdb645042d036
Felipe Alfonso González <f.alfonso.go@gmail.com>
2017-05-24 03:10:10 -0400

    v4.3.2.0.1

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
+//build in node module , no need to install it
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
