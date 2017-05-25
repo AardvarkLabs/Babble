09c80a7fc377b88f87ef6f1614a7bf1a98641cf6
Felipe Alfonso González <f.alfonso.go@gmail.com>
2017-05-25 01:19:41 -0400

    v4.3.0.1

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,4 +1,5 @@
 var generateMessage = (from, text) => {
+  // return an object 
   return {
     from,
     text,
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,9 +1,16 @@
-var expect = require('expect');
+var expect = require('expect'); // make an assertion
 
 var {generateMessage} = require('./message');
 
+// describe flag function
 describe('generateMessage', () => {
+
   it('should generate correct message object', () => {
+    // store res in var
+    // assert from match
+    // assert text match
+    //  assert createdAt is number 
+
     var from = 'Jen';
     var text = 'Some message';
     var message = generateMessage(from, text);
